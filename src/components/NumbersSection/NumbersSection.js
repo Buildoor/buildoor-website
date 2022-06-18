4ca53d7a6261dbddf79b4d3b0fb85d4cc183fac0
Ali Solanki <alisolanki123@gmail.com>
2022-06-18 12:33:50 +0530

    numbers section done

--- src/components/NumbersSection/NumbersSection.js
+++ src/components/NumbersSection/NumbersSection.js
@@ -4,35 +4,37 @@ import "./NumbersSection.css";
 export default function NumbersSection() {
   return (
     <div>
-      <h2>
-        Proof of Knowledge
-        <br />
-        that really matters
-      </h2>
-      <p>
-        <b>
-          While you are a BUILDoor, no one is really sure.
+      <div className="introSection">
+        <h2 className="heading">
+          Proof of Knowledge
           <br />
-          Earn credentials, certifications that top web3 recruiters care about.
-          Buildoor curates these challenges with the help of experts that
-          instantly recognize the hireability of someone who solves them.
-        </b>
-      </p>
+          that really matters
+        </h2>
+        <p className="details">
+          <b>
+            While you are a BUILDoor, no one is really sure.
+            <br />
+            Earn credentials, certifications that top web3 recruiters care
+            about. Buildoor curates these challenges with the help of experts
+            that instantly recognize the hireability of someone who solves them.
+          </b>
+        </p>
+      </div>
       <div className="numbers-list">
         <div className="numbers">
-          <text className="title">200K+</text>
+          <p className="title">200K+</p>
           <p>Total Students</p>
         </div>
         <div className="numbers">
-          <text className="title">30+</text>
+          <p className="title">30+</p>
           <p>Programs</p>
         </div>
         <div className="numbers">
-          <text className="title">9.1/10</text>
+          <p className="title">9.1/10</p>
           <p>Average Rating</p>
         </div>
         <div className="numbers">
-          <text className="title">400K+</text>
+          <p className="title">400K+</p>
           <p>Total Live Hours</p>
         </div>
       </div>
