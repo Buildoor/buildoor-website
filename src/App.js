5c14dd07363f1a6eb9ffa001272f02ec3582fd3f
Ali Solanki <alisolanki123@gmail.com>
2022-06-18 16:37:24 +0530

    metamask paused

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 
 function App() {
-  if (!window.ethereum) {
-    return <h1>Install Metamask</h1>;
-  } else {
-    return (
-      <div>
-        <LandingPage></LandingPage>
-      </div>
-    );
-  }
+  // if (!window.ethereum) {
+  //   return <h1>Install Metamask</h1>;
+  // } else {
+  return (
+    <div>
+      <LandingPage></LandingPage>
+    </div>
+  );
+  // }
 }
 
 export default App;
